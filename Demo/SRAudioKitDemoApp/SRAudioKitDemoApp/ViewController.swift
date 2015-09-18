50238c79ab1c449a91a7c8f0539b13cf40180150
Seorenn <hirenn@gmail.com>
2015-09-18 10:29:53 +0900

    Compositing Demo Project

---
+++ Demo/SRAudioKitDemoApp/SRAudioKitDemoApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SRAudioKitDemoApp
+//
+//  Created by Heeseung Seo on 2015. 9. 18..
+//  Copyright © 2015년 Seorenn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
