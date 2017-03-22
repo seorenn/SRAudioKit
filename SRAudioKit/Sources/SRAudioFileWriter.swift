d3ceb395c6531c0f08ec52636e2ec514fb21d8e4
Seorenn <hirenn@gmail.com>
2017-03-22 16:41:08 +0900

    Update indentation and loggings

--- SRAudioKit/Sources/SRAudioFileWriter.swift
+++ SRAudioKit/Sources/SRAudioFileWriter.swift
@@ -11,67 +11,70 @@ import AudioToolbox
 import SRAudioKitPrivates
 
 func SRAudioFileWriterOpen(_ path: String, fileType: SRAudioFileType, clientFormat: AudioStreamBasicDescription, fileFormat: AudioStreamBasicDescription) throws -> ExtAudioFileRef {
-    let url = URL(fileURLWithPath: path) as CFURL
-    let fileTypeID = fileType.audioFileTypeID
-    var fileRef: ExtAudioFileRef? = nil
-
-    var outputDesc = fileFormat
-
-    var size = UInt32(MemoryLayout<AudioStreamBasicDescription>.size)
-    var res = AudioFormatGetProperty(kAudioFormatProperty_FormatInfo, 0, nil, &size, &outputDesc)
-    guard res == noErr else {
-        throw SRAudioError.osStatusError(status: res, description: "AudioFormatGetProperty kAudioFormatProperty_FormatInfo")
-    }
-    
-    res = ExtAudioFileCreateWithURL(url, fileTypeID, &outputDesc, nil, AudioFileFlags.eraseFile.rawValue, &fileRef)
-    guard res == noErr else {
-        throw SRAudioError.osStatusError(status: res, description: "ExtAudioFileCreateWithURL")
-    }
-    
-    var cf = clientFormat
-    size = UInt32(MemoryLayout<AudioStreamBasicDescription>.size)
-    
-    res = ExtAudioFileSetProperty(fileRef!, kExtAudioFileProperty_ClientDataFormat, size, &cf)
-    guard res == noErr else {
-        throw SRAudioError.osStatusError(status: res, description: "ExtAudioFileSetProperty kExtAudioFileProperty_ClientDataFormat")
-    }
-    
-    return fileRef!
+  let url = URL(fileURLWithPath: path) as CFURL
+  let fileTypeID = fileType.audioFileTypeID
+  var fileRef: ExtAudioFileRef? = nil
+  
+  var outputDesc = fileFormat
+  
+  var size = UInt32(MemoryLayout<AudioStreamBasicDescription>.size)
+  var res = AudioFormatGetProperty(kAudioFormatProperty_FormatInfo, 0, nil, &size, &outputDesc)
+  guard res == noErr else {
+    throw SRAudioError.osStatusError(status: res, description: "AudioFormatGetProperty kAudioFormatProperty_FormatInfo")
+  }
+  print("SRAudioFileWriterOpen:")
+  print("- File Format: \(fileFormat)")
+  print("- Generated Output Description: \(outputDesc)")
+  
+  res = ExtAudioFileCreateWithURL(url, fileTypeID, &outputDesc, nil, AudioFileFlags.eraseFile.rawValue, &fileRef)
+  guard res == noErr else {
+    throw SRAudioError.osStatusError(status: res, description: "ExtAudioFileCreateWithURL")
+  }
+  
+  var cf = clientFormat
+  size = UInt32(MemoryLayout<AudioStreamBasicDescription>.size)
+  
+  res = ExtAudioFileSetProperty(fileRef!, kExtAudioFileProperty_ClientDataFormat, size, &cf)
+  guard res == noErr else {
+    throw SRAudioError.osStatusError(status: res, description: "ExtAudioFileSetProperty kExtAudioFileProperty_ClientDataFormat")
+  }
+  
+  return fileRef!
 }
 
 open class SRAudioFileWriter {
-    var fileRef: ExtAudioFileRef?
+  var fileRef: ExtAudioFileRef?
+  
+  public init?(clientFormat: AudioStreamBasicDescription, fileFormat: AudioStreamBasicDescription, fileType: SRAudioFileType, filePath: String) {
+    do {
+      print("SRAudioFileWriter INIT filePath[\(filePath)] clientFormat[\(clientFormat)] fileFormat[\(fileFormat)] fileType[\(fileType)]")
+      self.fileRef = try SRAudioFileWriterOpen(filePath, fileType: fileType, clientFormat: clientFormat, fileFormat: fileFormat)
+      //            try SRAudioFileWriterSetFileFormat(self.fileRef!, audioStreamDescription: audioStreamDescription)
+    }
+    catch let error as SRAudioError {
+      print("[SRAudioFileWriter.init] \(error)")
+      return nil
+    }
+    catch {
+      return nil
+    }
+  }
+  
+  open func close() throws {
+    guard let fileRef = self.fileRef else { return }
     
-    public init?(clientFormat: AudioStreamBasicDescription, fileFormat: AudioStreamBasicDescription, fileType: SRAudioFileType, filePath: String) {
-        do {
-            print("SRAudioFileWriter INIT filePath[\(filePath)] clientFormat[\(clientFormat)] fileFormat[\(fileFormat)] fileType[\(fileType)]")
-            self.fileRef = try SRAudioFileWriterOpen(filePath, fileType: fileType, clientFormat: clientFormat, fileFormat: fileFormat)
-//            try SRAudioFileWriterSetFileFormat(self.fileRef!, audioStreamDescription: audioStreamDescription)
-        }
-        catch let error as SRAudioError {
-            print("[SRAudioFileWriter.init] \(error)")
-            return nil
-        }
-        catch {
-            return nil
-        }
+    let res = ExtAudioFileDispose(fileRef)
+    guard res == noErr else {
+      throw SRAudioError.osStatusError(status: res, description: "SRAudioFileWriter.close()")
     }
+  }
+  
+  open func append(_ bufferList: UnsafePointer<AudioBufferList>, bufferSize: UInt32) throws {
+    guard let fileRef = self.fileRef else { return }
     
-    open func close() throws {
-        guard let fileRef = self.fileRef else { return }
-        
-        let res = ExtAudioFileDispose(fileRef)
-        guard res == noErr else {
-            throw SRAudioError.osStatusError(status: res, description: "SRAudioFileWriter.close()")
-        }
+    let res = ExtAudioFileWriteAsync(fileRef, bufferSize, bufferList)
+    guard res == noErr else {
+      throw SRAudioError.osStatusError(status: res, description: "SRAudioFileWriter.append()")
     }
- 
-    open func append(_ bufferList: UnsafePointer<AudioBufferList>, bufferSize: UInt32) throws {
-        guard let fileRef = self.fileRef else { return }
-        
-        let res = ExtAudioFileWriteAsync(fileRef, bufferSize, bufferList)
-        guard res == noErr else {
-            throw SRAudioError.osStatusError(status: res, description: "SRAudioFileWriter.append()")
-        }
-    }
+  }
 }
